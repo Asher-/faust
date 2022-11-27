d28b871e99f1f638f2ccdf6c5de11f27c75f6de8
Asher <asher@strong.ai>
2022-11-26 21:02:54 -0500

    Silenced ignored qualifier warnings

--- build/CMakeLists/configure/compiler/flags.cmake
+++ build/CMakeLists/configure/compiler/flags.cmake
@@ -2,7 +2,7 @@
 set (FAUST_DEFINITIONS ${FAUST_DEFINITIONS} -DLIBDIR="${LIBSDIR}")
 
 set (DEBUG_FLAGS "-fvisibility=hidden -fstandalone-debug -fno-eliminate-unused-debug-types -fdebug-macro -fno-limit-debug-info")
-set (WARNING_FLAGS "-Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-overloaded-virtual -Wno-shadow")
+set (WARNING_FLAGS "-Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-overloaded-virtual -Wno-shadow -Wno-ignored-qualifiers")
 
 set(CMAKE_CXX_FLAGS_DEBUG   "-g ${DEBUG_FLAGS} ${WARNING_FLAGS}")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fvisibility=hidden ${WARNING_FLAGS}")
